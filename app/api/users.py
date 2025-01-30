55b23f616204e8d52d80ed243c8cefa0eab773d8
Syshacker20 <kirildekster@gmail.com>
2025-01-30 16:54:39 +0300

    hjcv,

--- app/api/users.py
+++ app/api/users.py
@@ -2,6 +2,9 @@
 from app.schemas.user import UserCreate, UserResponse
 from app.crud.user import create_user
 from app.dependencies import get_current_user
+from typing import Annotated
+from fastapi import APIRouter
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -9,3 +12,20 @@ router = APIRouter()
 async def register_user(user: UserCreate):
     db_user = create_user(user)
     return db_user
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],) -> Token:
+    user = authenticate_user(form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
--- app/schemas/drive.py
+++ app/schemas/drive.py
@@ -1,1 +1,20 @@
-﻿
+﻿from datetime import datetime
+from pydantic import BaseModel
+
+class DriveCreate(BaseModel):
+    car_id: int  # ID автомобиля, который будет использоваться для поездки
+    event_volunteer_id: int  # ID записи EventVolunteer, связанной с поездкой
+    start_date_time: datetime  # Дата и время начала поездки
+    start_location: str  # Место отправления
+    max_passenger: int  # Максимальное количество пассажиров
+
+class DriveResponse(BaseModel):
+    id: int  # ID поездки
+    car_id: int  # ID автомобиля
+    event_volunteer_id: int  # ID записи EventVolunteer
+    start_date_time: datetime  # Дата и время начала поездки
+    start_location: str  # Место отправления
+    max_passenger: int  # Максимальное количество пассажиров
+
+    class Config:
+        from_attributes = True  # Ранее использовалось `orm_mode = True` в Pydantic v1
